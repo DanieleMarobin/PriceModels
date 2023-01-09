3492c88ce3294ae2cfaf76e1545a1878764c7e7a
DanieleMarobin <daniele.marobin@hotmail.com>
2023-01-09 12:12:10 +0100

    Added monthly heatmap settings

--- Home.py
+++ Home.py
@@ -10,18 +10,47 @@ if True:
     import Price_Models as mp
     import GDrive as gd
     import Charts as uc
-    import numpy as np
+    import func as fu
+
+# Functions
+if True:
+    def func_reset():
+        if ('df_model_all' in st.session_state):
+            del st.session_state['df_model_all']
+
+    def apply_func():
+        if ('heat_map_months' in st.session_state):
+            del st.session_state['heat_map_months']        
 
 # Preliminaries
 if True:
     st.set_page_config(page_title='Price Models',layout='wide',initial_sidebar_state='expanded')
+    st.markdown("### Price Models")
+    st.markdown("---")
+    st.sidebar.markdown("### Price Models")
+
     color_scales = uc.get_plotly_colorscales()
-    PM_DIR = 'Data/Models/Price Models/'
-    file_name = PM_DIR+'df_model_all.csv'
+
+
+# Filters and Settings
+if True:
+    st.sidebar.button('Get Latest File', on_click=func_reset)
+
+    with st.sidebar.form('run'):
+
+        # Top N Variables
+        top_n_vars = st.number_input('Top N Variables',1,100,20,1)
+
+        # Form Submit Button
+        st.form_submit_button('Apply',on_click=apply_func)
+
+font_size = st.sidebar.number_input('Font Size',5,20,8,1)
+chart_height = st.sidebar.number_input('Chart Height',100,2000,600,10)
+
 
 # Data
 if True:
-    df_model_all=gd.read_csv(file_name,parse_dates=['date'], dayfirst=True, index_col='date')
+    df_model_all=fu.get_data()
     model_df_instr=mp.model_df_instructions(df_model_all)
     model_df = mp.from_df_model_all_to_model_df(df_model_all, model_df_instr)
     # st.dataframe(model_df)
@@ -32,19 +61,24 @@ if True:
     # mask=((model_df.index.month<=4) | (model_df.index.month>=8))
     mask=(model_df.index.month>0)
     months=list(range(1,13))
-    heat_map_df=mp.heat_map_var_months(model_df[mask], y_cols=[y_col], top_n = 10, months=months, parallel=None, show=False)[y_col]
 
-    c=color_scales['RdBu-sequential']
+    if ('heat_map_months' in st.session_state):
+        # st.write('Getting the Monthly heatmap from memory')
+        heat_map_months=st.session_state['heat_map_months']
+    else:
+        # st.write('Calculating the Monthly heatmap')
+        st.session_state['heat_map_months']=mp.heat_map_var_months(model_df[mask], y_cols=[y_col], top_n = top_n_vars, months=months, parallel=None, show=False)[y_col]
+        heat_map_months=st.session_state['heat_map_months']
 
-    abs_max=heat_map_df['value'].abs().max() # so the positives are Blue and the negatives are red
-    # fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}',tickangle=-90)
-    fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}') #,tickangle=-90
+    abs_max=heat_map_months['value'].abs().max() # so the positives are Blue and the negatives are red
+
+    fig=uc.chart_heat_map(heat_map_months,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=color_scales['RdBu-sequential'], range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}') #,tickangle=-90
     fig.update_layout(coloraxis_showscale=False)
-    font_size=8
+    
     fig.update_layout(xaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
     fig.update_layout(yaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
 
     # fig.update_layout(width=1500,height=1500)
     # st.plotly_chart(fig)
-    selected_points = plotly_events(fig, override_height=600)
+    selected_points = plotly_events(fig, override_height=chart_height)
     st.write(selected_points)
---
+++ func.py
@@ -0,0 +1,25 @@
+
+import streamlit as st
+import GDrive as gd
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+
+def get_data():
+    PM_DIR = 'Data/Models/Price Models/'
+    file_name = PM_DIR+'df_model_all.csv'
+
+    # Delete all the items in Session state (to save memory)
+    for key in st.session_state.keys():
+        if ((key!='df_model_all') & (key!='heat_map_months')):
+            del st.session_state[key]
+
+    df_model_all=None
+    if ('df_model_all' in st.session_state):
+        df_model_all=st.session_state['df_model_all']
+    else:
+        st.write('Getting data from Google Drive')
+        st.session_state['df_model_all']=gd.read_csv(file_name,parse_dates=['date'], dayfirst=True, index_col='date')
+        df_model_all=st.session_state['df_model_all']
+    return df_model_all
