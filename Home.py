7b830dd50f0b2ff293f20f3456a4fde618472244
DanieleMarobin <daniele.marobin@hotmail.com>
2023-01-09 11:15:49 +0100

    Improve format

--- Home.py
+++ Home.py
@@ -32,26 +32,19 @@ if True:
     # mask=((model_df.index.month<=4) | (model_df.index.month>=8))
     mask=(model_df.index.month>0)
     months=list(range(1,13))
-    heat_map_df=mp.heat_map_var_months(model_df[mask], y_cols=[y_col], top_n = 40, months=months, parallel=None, show=False)[y_col]
+    heat_map_df=mp.heat_map_var_months(model_df[mask], y_cols=[y_col], top_n = 10, months=months, parallel=None, show=False)[y_col]
 
     c=color_scales['RdBu-sequential']
-    if False:
-        abs_max=heat_map_df['value'].abs().max() # so the positives are Blue and the negatives are red
-        heat_map=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, format_labels = '%{z:.1f}', title=y_col, range_color=(-abs_max,abs_max), tickangle=-90) 
-        x=['Jan', 'Feb', 'Mar','Apr', 'May', 'Jun','Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec', 'all']
-        heat_map.columns=heat_map.columns
-        heat_map.index=heat_map.index
-        fig = px.imshow(heat_map, color_continuous_scale=c) 
-    else:
-        abs_max=heat_map_df['value'].abs().max() # so the positives are Blue and the negatives are red
-        # fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}',tickangle=-90)
-        fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}') #,tickangle=-90
-        fig.update_layout(coloraxis_showscale=False)
-        font_size=8
-        fig.update_layout(xaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
-        fig.update_layout(yaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
-        # fig.update_layout(width=1500,height=1500)
 
+    abs_max=heat_map_df['value'].abs().max() # so the positives are Blue and the negatives are red
+    # fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}',tickangle=-90)
+    fig=uc.chart_heat_map(heat_map_df,x_col='report',y_col='v1',z_col='value', sort_by='all', transpose=True, color_continuous_scale=c, range_color=(-abs_max,abs_max), format_labels = '%{z:.1f}') #,tickangle=-90
+    fig.update_layout(coloraxis_showscale=False)
+    font_size=8
+    fig.update_layout(xaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
+    fig.update_layout(yaxis=dict(titlefont=dict(size=font_size),tickfont=dict(size=font_size)))
+
+    # fig.update_layout(width=1500,height=1500)
     # st.plotly_chart(fig)
-    selected_points = plotly_events(fig)
+    selected_points = plotly_events(fig, override_height=600)
     st.write(selected_points)
